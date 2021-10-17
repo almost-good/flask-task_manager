5b36e397c5cfe1e6dc1f59e2a4786739fc0e57c2
unknown <marja.marinic@gmail.com>
2021-10-17 22:40:09 +0100

    Add configuration

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
-# step 1
 import os
-from flask import Flask
+    # step 2 imports
+from flask_pymongo import PyMongo
+from flask import (
+    Flask, flash, render_template, 
+    redirect, request, session, url_for)
+from bson.objectid import ObjectId
+    # /step 2
 if os.path.exists("env.py"): # once its deployed on heroku then it wont be able to find the file, thats why if
     import env
 
 
 app = Flask(__name__) # instance of Flask
 
-@app.route("/") #test function 
-def hello():
-    return "Hello World ... again!"
+    # step 2 config
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+    # /step 2
+
+
+@app.route("/")
+@app.route("/get_tasks")
+def get_tasks():
+    tasks = mongo.db.tasks.find() 
+    return render_template("tasks.html", tasks=tasks)
+
 
 
 if __name__ == "__main__":  
